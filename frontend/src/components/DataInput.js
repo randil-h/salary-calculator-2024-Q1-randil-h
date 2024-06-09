bf591a45005e70023730f3a3033ba6d3cbb7de8b
randil-h <51442488+randil-h@users.noreply.github.com>
2024-06-09 10:51:28 +0530

    added static input fields

--- frontend/src/components/DataInput.js
+++ frontend/src/components/DataInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import reseticon from '../assets/icons/reseticon.png';
 import plussign from '../assets/icons/plussign.png';
+import crosssign from '../assets/icons/crosssign.png';
 
 const DataInput = () => {
     const [basicSalary, setBasicSalary] = useState('');
@@ -35,8 +36,9 @@ const DataInput = () => {
                     <input
                         type="number"
                         id="basicSalary"
-                        className="mt-1 block w-1/2 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
+                        className="mt-1 block w-2/3 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
                         value={basicSalary}
+                        placeholder= "Basic Salary"
                         onChange={(e) => setBasicSalary(e.target.value)}
                         required
                     />
@@ -45,15 +47,34 @@ const DataInput = () => {
                     <label htmlFor="earnings"
                            className="block text-sm font-medium text-gray-700"><strong>Earnings</strong></label>
                     <span className="text-customGray">Allowance, Fixed Allowance, Bonus and etc.</span>
-                    <input
-                        type="number"
-                        id="earnings"
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
-                        value={earnings}
-                        onChange={(e) => setEarnings(e.target.value)}
-                        required
-                    />
-                    <div className="flex items-center pt-4">
+                    <div className="flex items-center mt-1">
+                        <div className="w-1/3">
+                            <input
+                                type="text"
+                                id="earningsTitle"
+                                placeholder="Pay Details(Title)"
+                                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
+                                required
+                            />
+                        </div>
+                        <div className="w-1/3 ml-2">
+                            <input
+                                type="number"
+                                id="earningsValue"
+                                placeholder="Amount"
+                                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
+                                required
+                            />
+                        </div>
+                        <img src={crosssign} alt="Plus" className="mx-2"/>
+                        <input
+                            type="checkbox"
+                            id="terms"
+                            className="form-checkbox h-4 w-4 text-primaryBlue focus:ring focus:ring-primaryBlue"
+                        />
+                        <label htmlFor="terms" className="ml-2 text-gray-700 font-medium">EPF/ETF</label>
+                    </div>
+                    <div className="flex items-center mt-2">
                         <img src={plussign} alt="Plus" className="mr-2"/>
                         <button type="button" className="text-primaryBlue font-semibold">
                             Add New Allowance
@@ -65,14 +86,34 @@ const DataInput = () => {
                     <label htmlFor="deductions"
                            className="block text-sm font-medium text-gray-700"><strong>Deductions</strong></label>
                     <span className="text-customGray">Salary Advances, Loan Deductions and all</span>
-                    <input
-                        type="number"
-                        id="deductions"
-                        className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
-                        value={deductions}
-                        onChange={(e) => setDeductions(e.target.value)}
-                        required
-                    />
+                    <div className="flex items-center mt-1">
+                        <div className="w-1/3">
+                            <input
+                                type="text"
+                                id="deductionsTitle"
+                                placeholder="Deduction Details"
+                                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
+                                required
+                            />
+                        </div>
+                        <div className="w-1/3 ml-2">
+                            <input
+                                type="number"
+                                id="deductionsValue"
+                                placeholder = "Amount"
+                                className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-primaryBlue focus:ring focus:ring-primaryBlue focus:ring-opacity-50"
+                                value={deductions}
+                                required
+                            />
+                        </div>
+                        <img src={crosssign} alt="Plus" className="mx-2"/>
+                        <input
+                            type="checkbox"
+                            id="terms"
+                            className="form-checkbox h-4 w-4 text-primaryBlue focus:ring focus:ring-primaryBlue"
+                        />
+                        <label htmlFor="terms" className="ml-2 text-gray-700 font-medium">EPF/ETF</label>
+                    </div>
                     <div className="flex items-center pt-4">
                         <img src={plussign} alt="Plus" className="mr-2"/>
                         <button type="button" className="text-primaryBlue font-semibold">
